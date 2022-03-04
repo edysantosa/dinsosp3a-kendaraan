937951cb86b9cbf7abeab2130bf2bc2f549ca23f
Edy Santosa Putra <edy.santosa.p@gmail.com>
2022-03-04 10:11:03 +0800

    Kelompokkan layout

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="Neon is a bootstrap, laravel & php admin dashboard template">
+    <meta name="keywords" content="admin, admin dashboard, admin panel, admin template, admin theme, bootstrap 4, laravel, php, crm, analytics, responsive, sass support, ui kits, web app, clean design">
+    <meta name="author" content="Themesbox17">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+
+    <title>Neon - Bootstrap + Laravel + PHP Admin Dashboard Template</title>
+
+    <!-- Fevicon -->
+    <link rel="shortcut icon" href="dist/images/favicon.ico">
+    
+    <!-- Main App CSS -->
+    <link rel="stylesheet" href="{{ asset('dist/css/app.css') }}">
+
+</head>
+
+<body class="xp-horizontal">
+
+    <!-- Start XP Container -->
+    <div id="xp-container">
+
+        <!-- Start XP Rightbar -->
+        <div class="xp-rightbar">
+
+            <!-- Start XP Headerbar -->
+            <div class="xp-headerbar">
+
+                <!-- Start XP Topbar -->
+                <div class="xp-topbar">
+
+                    <!-- Start XP Row -->
+                    <div class="row"> 
+
+                        <!-- Start XP Col -->
+                        <div class="col-2 col-md-2 col-lg-2 align-self-center">
+                            <!-- Start XP Logobar -->
+                            <div class="xp-logobar">
+                                <a href="index.html" class="xp-small-logo"><img src="dist/images/mobile-logo.svg" class="img-fluid" alt="logo"></a>
+                                <a href="index.html" class="xp-main-logo"><img src="dist/images/logo.svg" class="img-fluid" alt="logo"></a>
+                            </div>                        
+                            <!-- End XP Logobar -->
+                        </div> 
+                        <!-- End XP Col -->
+
+                        @include('layouts.topmenu')
+
+                    </div> 
+                    <!-- End XP Row -->
+
+                </div>
+                <!-- End XP Topbar -->
+
+                @include('layouts.menu')
+
+            </div>
+            <!-- End XP Headerbar -->
+
+            @include('layouts.breadcrumb')
+            
+            <!-- Start XP Contentbar -->    
+            <div class="xp-contentbar">
+                @yield('content')
+            </div>
+            <!-- End XP Contentbar -->
+
+            <!-- Start XP Footerbar -->
+            <div class="xp-footerbar">
+                <footer class="footer">
+                    <p class="mb-0">© 2020 Neon - All Rights Reserved.</p>
+                </footer>
+            </div>
+            <!-- End XP Footerbar -->
+
+        </div>
+        <!-- End XP Rightbar -->        
+
+    </div>
+    <!-- End XP Container -->    
+
+    <!-- Main App JS -->        
+    <script src="{{ asset('dist/js/app.js') }}"></script>
+
+</body>
+</html>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,187 @@
+  <!-- Start XP Menubar -->                    
+  <div class="xp-menubar text-left">
+
+      <!-- Start XP Nav -->
+      <nav class="xp-horizontal-nav xp-mobile-navbar xp-fixed-navbar">
+
+          <div class="collapse navbar-collapse" id="navbar-menu">
+            <ul class="xp-horizontal-menu">
+
+              <li class="scroll"><a href="index.html"><i class="icon-speedometer"></i><span>Dashboard</span></a></li>
+              <li class="dropdown menu-item-has-mega-menu">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-layers"></i><span>UI Kits</span></a>
+                <div class="mega-menu dropdown-menu">
+                  <ul class="mega-menu-row" role="menu">
+                    <li class="mega-menu-col col-md-4">
+                      <ul class="sub-menu">
+                          <li><a href="ui-kits-alerts.html">Alerts</a></li>
+                          <li><a href="ui-kits-badges.html">Badges</a></li>
+                          <li><a href="ui-kits-buttons.html">Buttons</a></li>
+                          <li><a href="ui-kits-cards.html">Cards</a></li>
+                          <li><a href="ui-kits-carousel.html">Carousel</a></li>
+                          <li><a href="ui-kits-collapse.html">Collapse</a></li>
+                      </ul>
+                    </li>
+                    <li class="mega-menu-col col-md-4">
+                      <ul class="sub-menu">
+                          <li><a href="ui-kits-dropdowns.html">Dropdowns</a></li>
+                          <li><a href="ui-kits-grids.html">Grids</a></li>
+                          <li><a href="ui-kits-images.html">Images</a></li>
+                          <li><a href="ui-kits-media.html">Media</a></li>
+                          <li><a href="ui-kits-modals.html">Modals</a></li>                                
+                          <li><a href="ui-kits-paginations.html">Paginations</a></li>
+                      </ul>
+                    </li>
+                    <li class="mega-menu-col col-md-4">
+                      <ul class="sub-menu">
+                          <li><a href="ui-kits-popovers.html">Popovers</a></li>                                
+                          <li><a href="ui-kits-progressbars.html">Progress Bars</a></li>
+                          <li><a href="ui-kits-tabs.html">Tabs</a></li>    
+                          <li><a href="ui-kits-tooltips.html">Tooltips</a></li>
+                          <li><a href="ui-kits-typography.html">Typography</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-social-dropbox"></i><span>Components</span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="components-nestable.html">Nestable</a></li>
+                  <li><a href="components-ratings.html">Ratings</a></li>
+                  <li><a href="components-range-slider.html">Range Slider</a></li>
+                  <li><a href="components-switchery.html">Switchery</a></li>
+                  <li><a href="components-treeview.html">Tree View</a></li>
+                  <li><a href="components-widgets.html">Widgets</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-drop"></i><span>Icons</span></a>
+                <ul class="dropdown-menu">
+                      <li><a href="icon-material-design.html">Material Design</a></li>
+                      <li><a href="icon-font-awesome.html">Font Awesome</a></li>
+                      <li><a href="icon-simple-line.html">Simple Line Icons</a></li>
+                      <li><a href="icon-themify.html">Themify Icons</a></li>
+                      <li><a href="icon-typicons.html">Typicons</a></li>
+                      <li><a href="icon-ionicons.html">Ion Icons</a></li>
+                      <li><a href="icon-dripicons.html">Dripicons</a></li>
+                </ul>
+              </li>
+              <li class="scroll"><a href="events.html"><i class="icon-event"></i><span>Events</span></a></li>
+              <li class="dropdown menu-item-has-mega-menu">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-doc"></i><span>Forms</span></a>
+                <div class="mega-menu dropdown-menu">
+                  <ul class="mega-menu-row" role="menu">
+                    <li class="mega-menu-col col-md-4">
+                      <ul class="sub-menu">
+                          <li><a href="form-inputs.html">Form Inputs</a></li>
+                          <li><a href="form-groups.html">Form Groups</a></li>
+                          <li><a href="form-layouts.html">Form Layouts</a></li>
+                          <li><a href="form-validations.html">Form Validations</a></li>
+                          <li><a href="form-editors.html">Form Editors</a></li>
+                      </ul>
+                    </li>
+                    <li class="mega-menu-col col-md-4">
+                      <ul class="sub-menu">
+                          <li><a href="form-file-uploads.html">Form File Uploads</a></li>
+                          <li><a href="form-colorpickers.html">Form Color Pickers</a></li>
+                          <li><a href="form-datepickers.html">Form Date Pickers</a></li>
+                          <li><a href="form-maxlength.html">Form MaxLength</a></li>
+                          <li><a href="form-touchspin.html">Form Touchspin</a></li>
+                      </ul>
+                    </li>
+                    <li class="mega-menu-col col-md-4">
+                      <ul class="sub-menu">
+                          <li><a href="form-input-mask.html">Form Input Mask</a></li>
+                          <li><a href="form-selects.html">Form Selects</a></li>
+                          <li><a href="form-xeditable.html">Form X-editable</a></li>
+                          <li><a href="form-wizards.html">Form Wizards</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-graph"></i><span>Charts</span></a>
+                <ul class="dropdown-menu">
+                      <li><a href="chart-chartistjs.html">Chartist Chart</a></li> 
+                      <li><a href="chart-chartjs.html">Chartjs Chart</a></li>                                   
+                      <li><a href="chart-c3.html">C3 Chart</a></li>
+                      <li><a href="chart-flot.html">Flot Chart</a></li> 
+                      <li><a href="chart-morris.html">Morris Chart</a></li>                                
+                      <li><a href="chart-knob.html">Knob Chart</a></li>
+                      <li><a href="chart-piety.html">Piety Chart</a></li>
+                      <li><a href="chart-sparkline.html">Sparkline Chart</a></li>
+                </ul>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-options-vertical"></i><span>More</span></a>
+                <ul class="dropdown-menu">
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" >Email</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="email-inbox.html">Email Inbox</a></li>
+                      <li><a href="email-open.html">Email Open</a></li>
+                      <li><a href="email-compose.html">Email Compose</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Tables</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="table-bootstrap.html">Bootstrap Table</a></li>
+                      <li><a href="table-datatable.html">Data Table</a></li>
+                      <li><a href="table-editable.html">Editable Table</a></li>
+                      <li><a href="table-rwdtable.html">RWD Table</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Maps</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="map-google.html">Google Map</a></li>
+                      <li><a href="map-vector.html">Vector Map</a></li>
+                    </ul>
+                  </li>
+                </ul>
+              </li> 
+              <li class="dropdown menu-item-has-mega-menu">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-book-open"></i><span>Pages</span></a>
+                <div class="mega-menu dropdown-menu">
+                  <ul class="mega-menu-row" role="menu">
+                    <li class="mega-menu-col col-md-4">
+                      <ul class="sub-menu">
+                          <li><a href="page-login.html">Login</a></li>
+                          <li><a href="page-register.html">Register</a></li>
+                          <li><a href="page-forgotpsw.html">Forgot Password</a></li>
+                          <li><a href="page-lock-screen.html">Lock Screen</a></li> 
+                          <li><a href="page-comingsoon.html">Coming Soon</a></li>
+                      </ul>
+                    </li>
+                    <li class="mega-menu-col col-md-4">
+                      <ul class="sub-menu">
+                          <li><a href="page-maintenance.html">Maintenance</a></li>                               
+                          <li><a href="page-404.html">Error 404</a></li>  
+                          <li><a href="page-403.html">Error 403</a></li>
+                          <li><a href="page-500.html">Error 500</a></li>  
+                          <li><a href="page-503.html">Error 503</a></li>
+                      </ul>
+                    </li>
+                    <li class="mega-menu-col col-md-4">
+                      <ul class="sub-menu">
+                          <li><a href="page-starter.html">Starter Page</a></li>
+                          <li><a href="page-timeline.html">Timeline</a></li>
+                          <li><a href="page-pricing.html">Pricing</a></li>
+                          <li><a href="page-invoice.html">Invoice</a></li>
+                          <li><a href="page-faq.html">FAQ</a></li>
+                      </ul>
+                    </li>
+                  </ul>
+                </div>
+              </li> 
+            </ul>
+          </div>
+
+      </nav>
+      <!-- End XP Nav -->
+
+  </div>
+  <!-- End XP Menubar -->
