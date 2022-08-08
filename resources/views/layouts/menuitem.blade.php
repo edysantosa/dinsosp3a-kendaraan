eddc11295be54efc62588e3c8811b823bc67d10f
Edy Santosa Putra <edy.santosa.p@gmail.com>
2022-08-08 12:50:48 +0800

    Perbaiki menu

--- resources/views/layouts/menuitem.blade.php
+++ resources/views/layouts/menuitem.blade.php
@@ -5,18 +5,21 @@
     <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="{{ $menu->icon }}"></i><span>{{ $menu->menu_title }}</span></a>
     <ul class="dropdown-menu">
       @foreach($menu->children as $submenu)
-      <li class="@if ((count($submenu->children) > 0)) dropdown @endif">
-      <a href="{{ url($submenu->slug) }}" class="dropdown-toggle" data-toggle="dropdown">{{ $submenu->menu_title }}</a>
-        @if ((count($submenu->children) > 0))
+      @if ((count($submenu->children) > 0))
+        <li class="dropdown">
+          <a href="{{ url($submenu->slug) }}" class="dropdown-toggle" data-toggle="dropdown" >{{ $submenu->menu_title }}</a>
           <ul class="dropdown-menu">
             @foreach($submenu->children as $submenu2)
             <li><a href="{{ url($submenu2->slug) }}">{{ $submenu2->menu_title }}</a></li>
             @endforeach
           </ul>
-        @endif
+        </li>
+      @else
+        <li><a href="{{ url($submenu->slug) }}">{{ $submenu->menu_title }}</a></li>
+      @endif
       @endforeach
-    </ul>    
-  </li>
+    </ul>
+  </li> 
 @endif
 
 
